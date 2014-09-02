a1bffacaf3a4e6bdd76996629efad309732e790c
cebollinos <cebollinos@gmail.com>
2014-09-02 12:02:51 +0000

    added delat to glass platforms

--- platforms.js
+++ platforms.js
@@ -3,16 +3,27 @@ var _platformList = [
   {name:"fire",css_sprite:"pfire",hitplayer:kill_player,freq:50},
   {name:"railright",css_sprite:"railright",hitplayer:move_right,freq:90},
   {name:"railleft",css_sprite:"railleft",hitplayer:move_left,freq:90},
-  {name:"glass",css_sprite:"pgreen",hitplayer:activate_glass,freq:30}
+  {name:"glass",css_sprite:"pgreen",hitplayer:push_glass,freq:30}
   
 ];
-
+function push_glass(who)
+{
+  this.removeComponent("pgreen");
+  this.addComponent("pblue");
+  this.attr({count_down:20});
+  this.bind("EnterFrame", function (){ this.count_down--;
+                                     if (this.count_down===0)
+                                       {
+                                         this.hitplayer=activate_glass;
+                                         this.hitplayer();
+                                        
+                                       }});
+  
+}
 function activate_glass(who)
 {
-
- this.removeComponent("Platform");
- this.removeComponent("pgreen");
- this.addComponent("pblue");
+ this.removeComponent("Platform"); 
+ 
  this.bind("EnterFrame", function (){ this.y+=_pspeed*3;});
 }
 function move_right(who)
@@ -30,7 +41,7 @@ function kill_player(who){Kill(who);}
 
 function CreateRandomPlatform(height,canbeinvisible)
 {
-  var typeplat = roll(5)-1;
+  var typeplat = 4;//roll(5)-1;
   
   var tilesize = 32;
   var psize = roll(_size/32)*32;
@@ -43,7 +54,7 @@ function CreateRandomPlatform(height,canbeinvisible)
     .bind("EnterFrame",function() //normal platform movement
        {
          this.y-=this.speed;
-         if(this.y<0){
+         if(this.y<0 || this.y>_h+this.h+1){
            
          this.destroy();}
        })
