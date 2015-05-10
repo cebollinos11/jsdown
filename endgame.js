03f3232d7030a7f17e8bb9e6276a51b1d519aef6
cebollinos11 <cebollinos@gmail.com>
2015-05-10 11:46:07 +0200

    improved engame style

--- endgame.js
+++ endgame.js
@@ -13,8 +13,8 @@ function endGameClass()
 		
 		//Crafty.e("2D, DOM, Text").text("<H1><br><br><br><br><br><br><br><br> </H1>").attr({ x: 50, y: 100 ,w:500,h:400,z:1 });
 		Crafty.e("2D, DOM, Text")
-		.attr({ x: 100, y: 100 ,w:400 }).text("<div class='submitHSpanel'><p>Final Score: <span>"+G.score.score+
-			"</span></p><p>Name:<input id='submitName' ></input></p><button id='Send' class='jsButton' disabled='disabled'>Submit score</button><H1 id='Retry' class='jsButton'>Try AGAIN!</H1><H1 id='GoToMainMenu' class='jsButton'>Main menu</H1></div>")
+		.attr({ x: 100, y: 100 ,w:400 }).text("<div class='submitHSpanel'><div class='HSinfo'><p>Final Score: <span>"+G.score.score+
+			"</span></p><p>Name: <input id='submitName' ></input></p></div><button id='Send' disabled='disabled'>Submit score</button><H1 id='Retry' class='jsButton'>Try AGAIN!</H1><H1 id='GoToMainMenu' class='jsButton'>Main menu</H1></div>")
 		.attr({z:2});
 
 		  Crafty.e("2D, DOM, Text, HighScoresPanel").attr({ x: 570, y: 50 , w:200 })
