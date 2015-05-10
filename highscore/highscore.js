1a1681f1066d6f4df27fdfd4ada5247e803c7bfb
cebollinos11 <cebollinos@gmail.com>
2015-05-10 10:29:22 +0200

    improved table css

--- highscore/highscore.js
+++ highscore/highscore.js
@@ -1,3 +1,9 @@
+function getGetOrdinal(n) {
+   var s=["th","st","nd","rd"],
+       v=n%100;
+   return n+(s[(v-20)%10]||s[v]||s[0]);
+}
+
 function updateHS(){
     $(".HighScoresPanel").html("Loading...");
 
@@ -5,9 +11,9 @@ function updateHS(){
       var items = "";
       $.each( data["entries"], function( key, val ) {
         var n = key
-        items+=( "<tr><td>#"+parseInt(key+1)+"</td><td> " + val["name"] + "</td> <td>"+ val["score"]+"</td></tr>" );
+        items+=( "<tr><td>"+getGetOrdinal(parseInt(key+1))+"</td><td> " + val["name"] + "</td> <td>"+ val["score"]+"</td></tr>" );
       });
-    $(".HighScoresPanel").html("<h1>Online Leaderboard</h1><table>"+items+"</table>");
+    $(".HighScoresPanel").html("<h1>Online Leaderboard</h1><table class='hstable'>"+items+"</table>");
     
       console.log(items);
     });
