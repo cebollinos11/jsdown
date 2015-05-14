70327df65be71971b73e88b6a849b0f579145433
cebollinos11 <cebollinos@gmail.com>
2015-05-14 10:10:22 +0200

    tweaked score animation

--- miniscripts/ScoreParticle.js
+++ miniscripts/ScoreParticle.js
@@ -1,15 +1,15 @@
 
 function ScoreParticle(x,y,n)
 {
-	var P = Crafty.e('2D, Color, DOM, Text')
+	var P = Crafty.e('2D, DOM, Text')
 	.bind("EnterFrame", function (){
-
+		
   	this.y-=3;
   	this.TTL--;
   	if(this.TTL<0){this.destroy();}
     
     })
-	.text("+"+n)
+	.text('<div class="ScoreParticle">+'+n+'</div>')
 	.attr({x:x,y:y,TTL:20});
 
 }
