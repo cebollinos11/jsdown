e5c11912af0d20c137366df95ec0db4387aa4aac
cebollinos11 <cebollinos@gmail.com>
2015-05-17 14:59:19 +0200

    removed unused file

--- publish.sh
+++
@@ -1,8 +0,0 @@
-read -p "Enter commit text:  " yn;
-echo $yn;
-git init;
-git add .;
-git commit -m "$yn"
-git push origin master
-echo "Uploading to pablosan";
-scp -r ./ pablosan.com@ssh.pablosan.com:/www/jsdown
