630c970b421d6f38b96627fae2ccf9edb28cc37d
cebollinos11 <cebollinos@gmail.com>
2015-07-08 20:44:17 +0200

    added big fruits

--- miniscripts/fruits.js
+++ miniscripts/fruits.js
@@ -4,11 +4,18 @@ Fr = {fruitsize:32,
 	Frequency:45//in percentage
 	};
 function GenFruit(x,y){
+
+	var fsize = Fr.fruitsize;
+	var points = 25;
+	if(roll(50)==1){
+		fsize *=3;
+		points = 100;
+	}
 	
-	var x = x-Fr.fruitsize/2;
-	var h = y-Fr.fruitsize;
+	var x = x-fsize/2;
+	var h = y-fsize;
 	var FruitType = Fr.db[roll(4)-1];
-	var Fruit = Crafty.e('spawnable, '+FruitType+', 2D, Color, DOM, Collision').attr({x:x,y:h,w:Fr.fruitsize,h:Fr.fruitsize});
+	var Fruit = Crafty.e('spawnable, '+FruitType+', 2D, Color, DOM, Collision').attr({x:x,y:h,w:fsize,h:fsize,points:points});
 	 
 	Fruit.bind("EnterFrame", function (){
 		this.y-=_currPspeed;
@@ -21,7 +28,7 @@ function GenFruit(x,y){
 
 	Fruit.onHit('Player_element',function(who) {
 		playsound("bite");
-      	G.score.add(5*roll(10),who[0].obj);    	
+      	G.score.add(this.points,who[0].obj);    	
 		this.destroy();
 		
 	});
