dd43142f866b8c433817569a2dd1d30769b57ec8
El Sosi <cebollinos@gmail.com>
2025-08-14 22:32:38 +0200

    loli

--- miniscripts/fruits.js
+++ miniscripts/fruits.js
@@ -1,6 +1,6 @@
 
 Fr = {fruitsize:32,
-	db:["banana","cherry","pineapple","beer","burger","hotdog"],
+	db:["banana","cherry","pineapple","beer","burger","hotdog","lollipop"],
 	Frequency:70//in percentage
 	};
 function GenFruit(x,y){
@@ -14,7 +14,8 @@ function GenFruit(x,y){
 	
 	var x = x-fsize/2;
 	var h = y-fsize;
-	var FruitType = Fr.db[roll(6)-1];//Fr.db[roll(6)-1];
+	var FruitType = Fr.db[roll(Fr.db.length) - 1];
+	// FruitType = Fr.db[Fr.db.length - 1];
 	var Fruit = Crafty.e('spawnable, '+FruitType+', 2D, Color, DOM, Collision').attr({x:x,y:h,w:fsize,h:fsize,points:points});
 	 
 	Fruit.bind("EnterFrame", function (){
@@ -29,7 +30,7 @@ function GenFruit(x,y){
 	Fruit.onHit('Player_element',function(who) {
 		playsound("bite");
 		this.destroy();
-		if(this.has("hotdog") || this.has("burger")){
+		if(this.has("hotdog") || this.has("burger") || this.has("lollipop")){
 			who[0].obj.fat_component.getFat();
 			G.score.add(this.points*5,who[0].obj);    
 
