cf5953da3b7fc6cb2aad63345cf3591b2d0c4a66
El Sosi <cebollinos@gmail.com>
2025-08-15 21:53:33 +0200

    more

--- game.js
+++ game.js
@@ -10,17 +10,17 @@ function roll(max) //dice roll to simplify randomized calculations
 //Crafty.background('url(sprites/sky.png)');
 
 
-Crafty.scene("playgame", function() {  
+Crafty.scene("playgame", function () {
 
   //reset highscore
   _alreadySent = 0;
-  
+
   //reset alive players;
   _AlivePlayers = _nplayers;
-  
+
   //reset globals
-   _currPspeed = _pspeed;//
-  
+  _currPspeed = _pspeed;//
+
 
   _currFrequency = _frequency;
   //set background
@@ -29,71 +29,68 @@ Crafty.scene("playgame", function() {
   //Crafty.e("2D, DOM, background_component").attr({x: 0, y: 0, w: _w, h: _h,z:0})
   //.bind("EnterFrame", function (){this.y--;});
 
-  
+
   //put one plat
 
-  if(_test)
-    {RunTest();}
-  else{
+  if (_test) { RunTest(); }
+  else {
 
-    CreatePlatform(_h,1);
+    CreatePlatform(_h, 1);
 
   }
 
-   _pointsCounter = Crafty.e("2D, DOM, Text")
-    .attr({x: 20, y: _h*0.05, w: _w, h: 10})    
+  _pointsCounter = Crafty.e("2D, DOM, Text")
+    .attr({ x: 20, y: _h * 0.05, w: _w, h: 10 })
     .textFont({ size: '30px', weight: 'bold' })
-    .bind("EnterFrame", function (){    
-    _pointsCounter.text("Score: "+G.score.score+"<br>"+G.levelmgr.currentLevel.name).textFont({size:"25px", family:"Paragraph" });      
+    .bind("EnterFrame", function () {
+      _pointsCounter.text("Score: " + G.score.score + "<br>" + G.levelmgr.currentLevel.name).textFont({ size: "25px", family: "Paragraph" });
     });//.attr({ plat_frequency: _frequency });
-  
-    
+
+
   Crafty.e('DeathFloorBottom, 2D, DOM ')
-  .attr({x: 0, y: _h+50, w: _w, h: 10});
+    .attr({ x: 0, y: _h + 50, w: _w, h: 10 });
 
   G.spikes = TopLimit = Crafty.e('DeathFloorTopImage, 2D, DOM, Color, Tween')
-  .attr({x: 0, y: -30, w: _w, h: 64,z:1});
+    .attr({ x: 0, y: -30, w: _w, h: 64, z: 1 });
 
   var TopLimitEffect = Crafty.e('DeathFloorTop, 2D, DOM, Color')
-  .attr({x: 0, y: -50, w: _w, h: 64,z:1});
+    .attr({ x: 0, y: -50, w: _w, h: 64, z: 1 });
 
-  var TopSpikesSprite = Crafty.e("2D","Color","DOM","DeathFloorTopImage").color("black")
-  .attr({x: 0, y: -30-_h/2, w: _w, h: _h/2,z:1})
+  var TopSpikesSprite = Crafty.e("2D", "Color", "DOM", "DeathFloorTopImage").color("black")
+    .attr({ x: 0, y: -30 - _h / 2, w: _w, h: _h / 2, z: 1 })
 
-  TopLimit.attach(TopLimitEffect,TopSpikesSprite);
+  TopLimit.attach(TopLimitEffect, TopSpikesSprite);
 
 
 
   //.color('red');
 
   //STARTing Platforms
-  for(i=0;i<_np;i++)
-    {
-      //CreateRandomPlatform(_h*i/_np);
-    }
-  for(i=0;i<_nplayers;i++)
-    {_playerList[i].generate_player(i);}  
+  for (i = 0; i < _np; i++) {
+    //CreateRandomPlatform(_h*i/_np);
+  }
+  for (i = 0; i < _nplayers; i++) { _playerList[i].generate_player(i); }
 });
 
-function StartBackground(where){
+function StartBackground(where) {
   var initial_height = 0;
   //background_id = _currBG; //get random background
   background_id = G.levelmgr.currentLeveln;
-  background_string = "background_component"+String(background_id)
-  if(where===0) initial_height = _h;
-  Crafty.e('2D, DOM , '+background_string).attr({x: 0, y: where, w: _w, h: initial_height,z:-1})
-  .bind("EnterFrame", function (){
-    if(this.y === 0){ 
+  background_string = "background_component" + String(background_id)
+  if (where === 0) initial_height = _h;
+  Crafty.e('2D, DOM , ' + background_string).attr({ x: 0, y: where, w: _w, h: initial_height, z: -1 })
+    .bind("EnterFrame", function () {
+      if (this.y === 0) {
 
 
-      G.levelmgr.checkLevelUp(G.score.score);
-      StartBackground(_h-2);
+        G.levelmgr.checkLevelUp(G.score.score);
+        StartBackground(_h - 2);
 
-    }    
-    this.y-=1;
-    this.h++;
-    if(this.y===-_h) {this.destroy();}
-  });
+      }
+      this.y -= 1;
+      this.h++;
+      if (this.y === -_h) { this.destroy(); }
+    });
 }
 
 
@@ -101,55 +98,56 @@ function StartBackground(where){
 
 
 
-function RunSpikes(){
+function RunSpikes() {
   var spikes_speed = 5000;
-  playsound("alarm",0,0.2);
+  playsound("alarm", 0, 0.2);
 
   //add red warning
   G.spikes.redwarning = Crafty.e("2D,DOM,Color")
-  .attr({x:0,y:-_h*0.9,w:_w,h:_h,z:4,alpha:0,up:1})
-  .color("red")
-  .bind("EnterFrame",function(){
-    
-    this.alpha+=this.up/20;
-    if(this.alpha>0.3){this.up=-1;}
-    if (this.alpha<0) {this.up=1;};
+    .attr({ x: 0, y: -_h * 0.9, w: _w, h: _h, z: 4, alpha: 0, up: 1 })
+    .color("red")
+    .bind("EnterFrame", function () {
+
+      this.alpha += this.up / 20;
+      if (this.alpha > 0.3) { this.up = -1; }
+      if (this.alpha < 0) { this.up = 1; };
 
 
-  });
+    });
 
   G.spikes.attach(G.spikes.redwarning);
 
-  G.spikes.tween({y: _w*1/5}, spikes_speed);
-  G.spikes.bind("TweenEnd", function(){ 
-    G.spikes.tween({y:-30},spikes_speed/5); 
+  G.spikes.tween({ y: _w * 1 / 5 }, spikes_speed);
+  G.spikes.bind("TweenEnd", function () {
+    G.spikes.tween({ y: -30 }, spikes_speed / 5);
     console.log("TWEEN");
     G.spikes.unbind("TweenEnd");
-    playsound("alarm",0);
+    playsound("alarm", 0);
     G.spikes.redwarning.destroy();
 
   });
 
 }
 
-Crafty.c("DestroyOnSpikes",{
+Crafty.c("DestroyOnSpikes", {
 
-  init:function(){
+  init: function () {
 
 
-    this.onHit('DeathFloorTop',function () {
-      console.log(this,"on SPIKES");
+    this.onHit('DeathFloorTop', function () {
+      console.log(this, "on SPIKES");
       this.destroy();
 
-      try{        this.owner.umbrella = undefined;
-}
-      catch(err){}
+      try {
+        this.owner.umbrella = undefined;
+      }
+      catch (err) { }
 
 
-    }); 
+    });
 
   }
-  
+
 
 });
 
--- miniscripts/fruits.js
+++ miniscripts/fruits.js
@@ -1,48 +1,54 @@
+Fr = {
+    fruitsize: 32,
+    db: ["banana", "cherry", "pineapple", "beer", "burger", "hotdog", "lollipop", "icecream", "broccoli", "aubergine"],
+    Frequency: 70 // in percentage
+};
 
-Fr = {fruitsize:32,
-	db:["banana","cherry","pineapple","beer","burger","hotdog","lollipop","icecream","broccoli"],
-	Frequency:70//in percentage
-	};
-function GenFruit(x,y){
+function GenFruit(x, y) {
+    var fsize = Fr.fruitsize;
+    var points = 25;
 
-	var fsize = Fr.fruitsize;
-	var points = 25;
-	if(roll(50)==1){
-		fsize *=3;
-		points = 100;
-	}
-	
-	var x = x-fsize/2;
-	var h = y-fsize;
-	var FruitType = Fr.db[roll(Fr.db.length) - 1];
-	//  FruitType = Fr.db[Fr.db.length - 1];
-	var Fruit = Crafty.e('spawnable, '+FruitType+', 2D, Color, DOM, Collision').attr({x:x,y:h,w:fsize,h:fsize,points:points});
-	 
-	Fruit.bind("EnterFrame", function (){
-		this.y-=_currPspeed;
-         if(this.y<-this.h || this.y>_h+this.h+1){           
-         this.destroy();
-         
+    if (roll(50) == 1) {
+        fsize *= 3;
+        points = 100;
+    }
 
-     	}
-	});
+    var x = x - fsize / 2;
+    var h = y - fsize;
+    var FruitType = Fr.db[roll(Fr.db.length) - 1];
 
-	Fruit.onHit('Player_element',function(who) {
-		playsound("bite");
-		this.destroy();
-		if(this.has("hotdog") || this.has("burger") || this.has("lollipop")|| this.has("icecream")){
-			who[0].obj.fat_component.getFat();
-			G.score.add(this.points*5,who[0].obj);    
+    var Fruit = Crafty.e('spawnable, ' + FruitType + ', 2D, Color, DOM, Collision, Tween')
+        .attr({ x: x, y: h, w: fsize, h: fsize, points: points });
 
-		}
-		else{
-			
-      	G.score.add(this.points,who[0].obj);   
-      	if (roll(3)<4) {
-				who[0].obj.fat_component.removeFat();
-			} 	
-		}
-		
-	});
+    Fruit.bind("EnterFrame", function () {
+        this.y -= _currPspeed;
+        if (this.y < -this.h || this.y > _h + this.h + 1) {
+            this.destroy();
+        }
+    });
+
+    Fruit.onHit('Player_element', function (who) {
+        playsound("bite");
+
+        // Prevent multiple hits triggering multiple animations
+        if (this._eaten) return;
+        this._eaten = true;
+
+        // Score & fat logic
+        if (this.has("hotdog") || this.has("burger") || this.has("lollipop") || this.has("icecream")) {
+            who[0].obj.fat_component.getFat();
+            G.score.add(this.points * 5, who[0].obj);
+        } else {
+            G.score.add(this.points, who[0].obj);
+            if (roll(3) < 4) {
+                who[0].obj.fat_component.removeFat();
+            }
+        }
+
+        // Animate shrink to 0 over 0.5 sec (500ms)
+        this.tween({ w: 0, h: 0, x: this.x + this.w / 2, y: this.y + this.h / 2 }, 300) // 30 frames ≈ 0.5 sec
+            .bind("TweenEnd", function () {
+                this.destroy();
+            });
+    });
 }
-
